f68bc6ee3eb0c9c5fff8e0e457e963e9dcddd362
ThuHa <thuha2003ha@gmail.com>
2024-11-20 11:49:18 +0700

    th

--- app/src/main/java/com/example/appbook/MainTrangChu.java
+++ app/src/main/java/com/example/appbook/MainTrangChu.java
@@ -1,4 +1,28 @@
 package com.example.appbook;
 
-public class MainTrangChu {
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainTrangChu extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) { // Đổi Oncreate thành onCreate
+        super.onCreate(savedInstanceState); // Đổi saveInstanceState thành savedInstanceState
+        setContentView(R.layout.trangchu);
+    }
 }
+
+
+
+//public class MainDangKy extends Activity {
+//    EditText etUsername, etPhone, etPassword, etPasswordAgain;
+//    Button btnCreateAccount;
+//    ImageView backButton;
+//    MyDatabaseHelper databasedoctruyen;
+//
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.dangky);
+
